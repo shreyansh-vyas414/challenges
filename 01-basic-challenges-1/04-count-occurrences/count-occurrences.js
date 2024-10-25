722242e591aa2923a680ba63ad3c0dcf5518e9d9
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2024-10-25 22:47:36 +0530

    solved count occourences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,10 @@
-function countOccurrences() {}
+function countOccurrences(s, c) {
+  let v = 0;
+  for (let i = 0; i < s.length; i++) {
+    const e = s[i];
+    if (e === c) v++;
+  }
+  return v;
+}
 
 module.exports = countOccurrences;
