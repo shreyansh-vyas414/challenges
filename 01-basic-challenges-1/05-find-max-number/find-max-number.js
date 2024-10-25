550ee214c066a943aa323b5e82a5b15d43d4fcf6
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2024-10-25 23:31:23 +0530

    solved find max number :rainbow:

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,24 @@
-function findMaxNumber() {}
+function findMaxNumber(a) {
+  // Using simple for loop
+  let l = a.length;
+  let c = [0];
+  for (let i = 0; i < l; i++) a[i] > c ? (c = a[i]) : null;
+  return c;
+  // Using forEach Loop
+  //   let c = [0];
+  //   a.forEach((e) => (e > c ? (c = e) : null));
+  //   console.log(c);
+  //   let c = [0];
+  //   let l = a.length;
+  //   let i = 0;
+  //   while (i < l) {
+  //     a[i] > c ? (c = a[i]) : null;
+  //     i++;
+  //   }
+  //   return c
+  // Using reduce
+  //   let max = a.reduce((e, val) => (val > e ? val : e), 0);
+  console.log(max);
+}
 
 module.exports = findMaxNumber;
